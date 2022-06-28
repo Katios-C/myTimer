5f1b41994a79b86664e4b2fc9910dddfa1f595d5
Katios <kate.chonkina@gmail.com>
2022-06-28 10:21:02 +0600

    1

---
+++ RandomTimer/RandomTimer WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  RandomTimer WatchKit Extension
+//
+//  Created by Екатерина Чернова on 28.06.2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
---
+++ RandomTimer/RandomTimer/RandomTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTimerApp.swift
+//  RandomTimer
+//
+//  Created by Екатерина Чернова on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
